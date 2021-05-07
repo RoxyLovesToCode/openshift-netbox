efd6622dcac7e5e7362508b59ca4161e2d9edd12
RoxyLovesToCode <66651349+RoxyLovesToCode@users.noreply.github.com>
2021-05-08 05:56:51 +1200

    Update assemble.sh

--- openshift/s2i/assemble.sh
+++ openshift/s2i/assemble.sh
@@ -20,10 +20,10 @@ pip3 install \
     social-auth-app-django==3.1.0
 
 # Custom settings.py
-mv netbox/netbox/settings.py netbox/netbox/upstream_settings.py
-cp /opt/app-root/etc/settings.py netbox/netbox/settings.py
-cp /opt/app-root/etc/openshift_urls.py netbox/netbox/openshift_urls.py
-cp /opt/app-root/etc/openshift_middleware.py netbox/netbox/openshift_middleware.py
-cp /opt/app-root/etc/openshift_auth.py netbox/netbox/openshift_auth.py
+# mv netbox/netbox/settings.py netbox/netbox/upstream_settings.py
+# cp /opt/app-root/etc/settings.py netbox/netbox/settings.py
+# cp /opt/app-root/etc/openshift_urls.py netbox/netbox/openshift_urls.py
+# cp /opt/app-root/etc/openshift_middleware.py netbox/netbox/openshift_middleware.py
+# cp /opt/app-root/etc/openshift_auth.py netbox/netbox/openshift_auth.py
 
 fix-permissions /opt/app-root
