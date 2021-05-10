ccf477e08c3ed8d5923080d6a415260fe3efe475
RoxyLovesToCode <66651349+RoxyLovesToCode@users.noreply.github.com>
2021-05-10 15:25:46 +1200

    Update run.sh

--- openshift/s2i/run.sh
+++ openshift/s2i/run.sh
@@ -50,8 +50,8 @@ fi
 
 # settings.py needs to be importable for collectstatic to work, so we either need to
 # import the netbox-config ConfigMap during build or run collectstatic at runtime.
-echo "---> Collecting Django static files ..."
-python3 ${manage_file} collectstatic --noinput
+#echo "---> Collecting Django static files ..."
+#python3 ${manage_file} collectstatic --noinput
 
 export WEB_CONCURRENCY=${WEB_CONCURRENCY:-$(get_default_web_concurrency)}
 
